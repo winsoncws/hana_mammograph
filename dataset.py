4f3d521480fdc092ef7e7cc31e30c7e569ab5003
Isaiah <isaiah.chua@gmail.com>
2023-01-05 15:39:24 +0800

    helper module for creating datasets

---
+++ dataset.py
@@ -0,0 +1,25 @@
+import sys, os
+import glob
+import numpy as np
+import torch
+import pydicom
+from torch.utils.data import Dataset, DataLoader
+
+class MammoData(Dataset):
+
+    def __init__(self, datadir):
+        super().__init__()
+        self.datadir = os.path.abspath(datadir)
+        self.datafiles = glob.glob(os.path.join(self.datadir, "**/*.dcm"),
+                                                recursive=True)
+
+    def __len__(self):
+        print(len(self.datafiles))
+
+    def __getitem__(self, i):
+        filepath = self.datafiles[i]
+        ds = pydicom.dcmread(filepath)
+        return ds
+
+if __name__ == "__main__":
+    pass
