17341adef127520938d19e9da5d9856d93bec5f0
Isaiah <isaiah.chua@gmail.com>
2023-01-17 00:32:17 +0800

    separated train test split from preprocessing so it can be executed separately from data creation

--- isaiah/train_test_split.py
+++ isaiah/train_test_split.py
@@ -1,15 +1,18 @@
 import sys, os
 from glob import glob
 import json
+import yaml
 import numpy as np
+import pandas as pd
+from addict import Dict
 from sklearn.model_selection import train_test_split
 
 if __name__ == "__main__":
     output = {}
-    dataDir = os.path.abspath(sys.argv[1])
-    saveFile = sys.argv[2]
-    dataFiles = sorted(glob(os.path.join(dataDir, "*.npz"), recursive=True))
-    output["Train"], output["Test"] = train_test_split(dataFiles, test_size=0.2)
-    with open(saveFile, "w") as f:
+    config_file = os.path.abspath(sys.argv[1])
+    cfgs = Dict(yaml.load(open(config_file, "r"), Loader=yaml.Loader))
+    md = pd.read_json(cfgs.metadata_dest, orient="index", convert_axes=False, convert_dates=False)
+    output["Train"], output["Test"] = train_test_split(md.index, test_size=cfgs.test_size)
+    with open(cfgs.traintest_dest, "w") as f:
         json.dump(output, f)
 
