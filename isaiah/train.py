096b431ed2d5c88cf9b96e12328aeb8dd7ac6296
Isaiah <isaiah.chua@gmail.com>
2023-01-17 13:39:04 +0800

    Group sampler to allow use of __getitem__ in the pytorch dataset to accept two arguments.

--- isaiah/train.py
+++ isaiah/train.py
@@ -5,10 +5,10 @@ import torch
 import torch.nn as nn
 from torch.optim import Adam
 from torch.utils.data import DataLoader
-from torchio.data import dataset
-from build import MammoH5Data
+from build import MammoH5Data, GroupSampler
 from build import DenseNet
 from build import PFbeta
+import json
 import yaml
 from addict import Dict
 
@@ -21,53 +21,94 @@ class Train:
         self.optimizer_cfgs = cfgs.optimizer_params
         self.data_cfgs = cfgs.dataset_params
         self.train_cfgs = cfgs.train_params
-        self.labels = self.data_cfgs.label
+
+        if not self.train_cfgs.no_gpu and torch.cuda.is_available():
+            self.device = torch.device('cuda')
+        elif not self.train_cfgs.no_gpu and torch.backends.mps.is_available():
+            self.device = torch.device("mps")
+        else:
+            self.device = torch.device('cpu')
+
         self.epochs = self.train_cfgs.epochs
-        self.savepath = abspath(self.train_cfgs.savepath)
+        self.model_path = abspath(self.train_cfgs.savepath)
+
+        self.labels = self.data_cfgs.label
         self.loss_weight_map = {}
         for i, key in enumerate(self.labels):
             self.loss_weight_map[key] = self.train_cfgs.loss_weights[i]
         self.best_weights = None
 
-        self.data = MammoH5Data(self.data_cfgs)
-        self.trainloader = DataLoader(self.data, 10, shuffle=True)
-        self.validloader = DataLoader(self.data)
+        self.data = MammoH5Data(self.device, self.data_cfgs)
+        self.traintest_path = self.data_cfgs.traintest_path
+        with open(self.traintest_path, "r") as f:
+            self.traintestsplit = Dict(json.load(f))
+        self.train_sampler = GroupSampler(self.traintestsplit.train, shuffle=True)
+        self.val_sampler = GroupSampler(self.traintestsplit.validation, shuffle=True)
+        self.batch_size = self.train_cfgs.batch_size
+        self.val_size = self.train_cfgs.validation_size
+        self.trainloader = DataLoader(self.data, self.batch_size, sampler=self.train_sampler)
+        self.validloader = DataLoader(self.data, self.val_size, sampler=self.val_sampler)
 
-    def _save(self):
+        self.train_report = Dict({
+            "loss": [],
+            "score": []
+        })
+        self.train_report_path = cfgs.train_cfgs.report_path
+
+    def _CheckMakeDirs(self, filepath):
+        if not isdir(dirname(filepath)):
+            os.makedirs(dirname(filepath))
+
+    def _SaveBestModel(self):
         if self.best_weights != None:
-            if not isdir(dirname(self.savepath)):
-                os.makedirs(dirname(self.savepath))
-            torch.save(self.best_weights, self.savepath)
+            self._CheckMakeDirs(self.model_path)
+            torch.save(self.best_weights, self.model_path)
+
+    def SaveTrainingReport(self):
+        self._CheckMakeDirs(self.train_report_path)
+        with open(self.train_report_path, "w") as f:
+            f.write(self.train_report)
+
+    def GetTrainingReport(self):
+        return self.train_report
 
     def TrainDenseNet(self):
+        self.best_weights = None
         self.model = DenseNet(self.model_cfgs)
-        self.optimizer = Adam(self.model.parameters, cfgs.optim_params)
+        self.model.to(self.device)
+        self.optimizer = Adam(self.model.parameters(), **cfgs.optim_params)
         met_name = "PFbeta"
         a = torch.from_numpy(np.array([self.loss_weight_map[key] for key in self.labels],
                                                dtype=np.float32))
         c1 = nn.BCELoss(a[:4])
         c2 = nn.L1Loss(a[4:])
-        val_count = 0
         loss = torch.tensor(0.)
         best_score = 0.
         for epoch in range(1, self.epochs + 1):
-            img_id, inp, gt = next(iter(self.trainloader))
-            out = self.model(inp)
-            loss = c1(out[:4], gt[:4]) + c2(out[4:], gt[4:])
-            loss.backward()
-            print(f"epoch {epoch}, loss: {loss.item()}")
-            if epoch % 10 == 0:
-                val_count += 1
-                self.model.eval()
-                img_id, vi, vt = next(iter(self.validloader))
-                preds = self.model(vi)
-                sco = PFbeta(preds[3].numpy(), vt[3])
-                print(f"Validation {val_count}, {met_name}: {sco}")
-                if sco > best_score:
-                    self.best_weights = self.model.state_dict()
-                    best_score = sco
-                    self._save()
-                    print("Better model found and saved")
+
+            # Training loop
+            self.model.train()
+            for batch, (img_id, inp, gt) in enumerate(self.trainloader):
+                out = self.model(inp)
+                loss = c1(out[:4], gt[:4]) + c2(out[4:], gt[4:])
+                self.train_report.append(loss)
+                self.optimizer.zero_grad()
+                loss.backward()
+                self.optimizer.step()
+                print(f"batch {batch}, batch_size: {self.batch_size}, loss: {loss.item()}")
+
+            # Validation loop;  every epoch
+            self.model.eval()
+            img_id, vi, vt = next(iter(self.validloader))
+            preds = self.model(vi)
+            sco = PFbeta(preds.item()[3], vt.item()[3], beta=0.5)
+            self.train_report.score.append(sco)
+            print(f"epoch: {epoch}, loss: {loss.item()}, {met_name}: {sco}")
+            if sco > best_score:
+                self.best_weights = self.model.state_dict()
+                best_score = sco
+                self._SaveBestModel()
+                print("Better model found and saved")
         print(f"total epochs: {self.epochs}, final_loss: {loss.item()}, best_score: {best_score}.")
         return
 
